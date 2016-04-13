6e959efc3169de58e379cdb583b59e99b2a436c0
Oliver Stigley <oliverstigley@gmail.com>
2016-04-14 11:05:09 +1200

    operator replace

--- calculator.js
+++ calculator.js
@@ -1,16 +1,32 @@
 var inputStr = "";
 var values = [];
+var output;
+var displayStr = "";
 
 $("button").click(function(){
 	inputStr += this.value;
 	if(this.value == "C") {
 		clear();
+	} else if(this.value == "=") {
+//		calculate(inputStr);
+//	$('#outPut').html(output);
+
 	}
+	
 	$('#outPut').html(inputStr);
+
 });
 
 var clear = function() {
 
 	inputStr = "";
+}
+
+var calculate = function(input){
+		output = eval(input);
+};
+
+var display = function(myStr){
+	myStr.replace(/[/]/g, "÷");
 }
 
